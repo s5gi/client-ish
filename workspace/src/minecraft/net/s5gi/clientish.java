f49e79c212758dd2350759e9cc6c9c7baea50f50
5gi ☑ <5gi.exist@gmail.com>
2023-11-01 18:12:46 -0400

    forgot to change version tagdhfgklshdfglk

--- workspace/src/minecraft/net/s5gi/clientish.java
+++ workspace/src/minecraft/net/s5gi/clientish.java
@@ -6,11 +6,10 @@ public class clientish {
     public static String name = "Client-ish";
 
     //2NDHALFYEAR-1STHALFYEAR.DAYOFMONTH.MONTH
-    public static String version = "2320.31.10-alpha";
+    public static String version = "2320.01.11-alpha";
 
     public static void startup() {
-        Minecraft mc = Minecraft.getMinecraft();
-        mc.setTitle("Minecraft | " + name + " v" + version);
+        Minecraft.setTitle("Minecraft | " + name + " v" + version);
     }
     public static void shutdown() {
 
